112a5573c316f063b64519ebc53dd2030f64504c
Abraao <abraao@urbbox.com.br>
2016-01-04 14:56:35 +0100

    apps

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -65,6 +65,75 @@
 				</div>
 			</section>
 		</header>
+		<section class="features-list" id="features">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="col-md-4 feature-1 wp2">
+							<div class="feature-icon">
+								<i class="fa fa-desktop"></i>
+							</div>
+							<div class="feature-content">
+								<h1>Cockpit</h1>
+								<p>Built using HTML5/CSS3 and jQuery, and built using one of the world's most powerful CSS frameworks available, Bootstrap.</p>
+								<a href="http://tympanus.net/codrops/?p=22554" class="read-more-btn">Read More <i class="fa fa-chevron-circle-right"></i></a>
+							</div>
+						</div>
+						<div class="col-md-4 feature-2 wp2">
+							<div class="feature-icon">
+								<i class="fa fa-flash"></i>
+							</div>
+							<div class="feature-content">
+								<h1>7W Capture</h1>
+								<p>Perfect if you run your own start-up, product or service. Boxify can showcase your business converting your visits to income.</p>
+								<a href="http://tympanus.net/codrops/?p=22554" class="read-more-btn">Download <i class="fa fa-chevron-circle-right"></i></a>
+							</div>
+						</div>
+						<div class="col-md-4 feature-2 wp2">
+							<div class="feature-icon">
+								<i class="fa fa-flash"></i>
+							</div>
+							<div class="feature-content">
+								<h1>IDA </h1>
+								<p>Perfect if you run your own start-up, product or service. Boxify can showcase your business converting your visits to income.</p>
+								<a href="http://tympanus.net/codrops/?p=22554" class="read-more-btn">Download <i class="fa fa-chevron-circle-right"></i></a>
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="features-list" id="features">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="col-md-6 feature-5 wp2 ">
+							<div class="feature-icon">
+								<i class="fa fa-heart"></i>
+							</div>
+							<div class="feature-content">
+								<h1>Wba Jahrestreffen</h1>
+								<p>As aways, download Boxify for free exclusively from Codrops. If you love Boxify and want to thank me, simply <a href="http://peterfinlan.com/">buy me a beer</a>. </p>
+								<a href="http://tympanus.net/codrops/?p=22554" class="read-more-btn">Download <i class="fa fa-chevron-circle-right"></i></a>
+							</div>
+						</div>
+						<div class="col-md-6 feature-6 wp2">
+							<div class="feature-icon">
+								<i class="fa fa-heart"></i>
+							</div>
+							<div class="feature-content">
+								<h1>Werkzeugbau Kolloquium</h1>
+								<p>As aways, download Boxify for free exclusively from Codrops. If you love Boxify and want to thank me, simply <a href="http://peterfinlan.com/">buy me a beer</a>. </p>
+								<a href="http://tympanus.net/codrops/?p=22554" class="read-more-btn">Read More <i class="fa fa-chevron-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+				
+				</div>
+			</div>
+		</section>
 		<section class="video" id="about">
 			<div class="container">
 				<div class="row">
