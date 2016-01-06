061983d7f6666b3f4f3ccdc314abd77672a8cb83
Luiz Gabriel <luiz.gabriel.cristo@hotmail.com>
2016-01-06 17:15:46 -0200

    Removed CSRF Protection

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Wba\Http\Middleware\VerifyCsrfToken::class,
+        //\Wba\Http\Middleware\VerifyCsrfToken::class,
     ];
 
     /**
